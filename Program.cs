a17d667e6b09e19cc2d3d66faff8448a0183a8df
Jorgyg <jgraslorenzo@gmail.com>
2023-09-12 17:25:20 +0200

    API

---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+builder.WebHost.UseUrls($"http://*:{port};");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection(); //For development purpose, comment out the line that forces HTTPS redirection in your `Program.cs`.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
